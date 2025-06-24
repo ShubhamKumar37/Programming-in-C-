a2d84b4ff8516067943540c5f99d4a738afb4283
ShubhamKumar37 <sk9818281820@gmail.com>
2025-06-24 12:41:10 +0530

    basic programming is completed now moving with c# advance OOP

--- FunWithTuples/FunWithTuples/Program.cs
+++ FunWithTuples/FunWithTuples/Program.cs
@@ -23,3 +23,94 @@ var foo = new{prop1= 323, prop2= "Shubham"};
 var tup = (foo.prop1,  foo.prop2);
 Console.WriteLine($"This is the value = {tup.Item1}");
 Console.WriteLine($"This is the value = {tup.Item2}");
+
+
+Console.WriteLine("=> Tuples Equality/Inequality");
+// lifted conversions
+var left = (a: 5, b: 10);
+(int? a, int? b) nullableMembers = (5, 10);
+Console.WriteLine(left == nullableMembers); // Also true
+                                            // converted type of left is (long, long)
+(long a, long b) longTuple = (5, 10);
+Console.WriteLine(left == longTuple); // Also true
+                                      // comparisons performed on (long, long) tuples
+(long a, int b) longFirst = (5, 10);
+(int a, long b) longSecond = (5, 10);
+Console.WriteLine(longFirst == longSecond); // Also true
+//Console.WriteLine((1, 2, 3) == ("Shubham")); // This will be compilation
+
+static (int a, string b, int c) FillValues()
+{
+    return (32, "Shubham", 34);
+}
+
+var sample = FillValues();
+Console.WriteLine("{1} {0} {2}", sample.a, sample.b, sample.c);
+
+//var (first, _, last) = SplitNames("Philip F Japikse");
+//Console.WriteLine($"{first}:{last}");
+
+// Switch case
+switch( "rock", "paper")
+{
+    case ("rock", "paper"):
+        Console.WriteLine("You win");
+        break;
+    default:
+        Console.WriteLine("Every combination is done");
+        break;
+}
+
+//Switch expression with Tuples
+static string RockPaperScissors(string first, string second)
+{
+    return (first, second) switch
+    {
+        ("rock", "paper") => "Paper wins.",
+        ("rock", "scissors") => "Rock wins.",
+        ("paper", "rock") => "Paper wins.",
+        ("paper", "scissors") => "Scissors wins.",
+        ("scissors", "rock") => "Rock wins.",
+        ("scissors", "paper") => "Scissors wins.",
+        (_, _) => "Tie.",
+    };
+}
+Console.WriteLine(RockPaperScissors("fsd", "sdf"));
+
+(int x, int y) myTup = (32, 5);
+(int a, int b) = myTup;
+
+Console.WriteLine("This is the value of a {0} and this is for b {1}", a, b);
+
+Point p = new Point(32, -54);
+//(int n1, int n2) = p.Deconstruct();
+(int n1, int n2) = p.Deconstruct();
+
+Console.WriteLine(WhichPlane(p));
+static string WhichPlane(Point p)
+{
+    return p.Deconstruct() switch // We can only just use p instead of calling the whole method but idk, here it is not working
+    {
+        (0, 0) => "Center",
+        var (x, y) when x > 0 && y > 0 => "One",
+        var (x, y) when x < 0 && y > 0 => "Two",
+        var (x, y) when x > 0 && y < 0 => "Three",
+        var (x, y) when x < 0 && y < 0 => "Four",
+        (_, _) => "Border"
+
+    };
+}
+
+struct Point
+{
+    public int x;
+    public int y;
+
+    public Point(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public (int a, int b) Deconstruct() => (x, y);
+}
