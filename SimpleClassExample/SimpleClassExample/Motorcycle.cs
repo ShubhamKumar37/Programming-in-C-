d34826f5d39c43bcb03bb292a9d4a8d1ab820a61
ShubhamKumar37 <sk9818281820@gmail.com>
2025-06-24 23:54:27 +0530

    Completed constructor and now moving to the static keyword concept

--- SimpleClassExample/SimpleClassExample/Motorcycle.cs
+++ SimpleClassExample/SimpleClassExample/Motorcycle.cs
@@ -3,13 +3,26 @@ class Motorcycle
 {
     public int driverIntensity;
     public string driverName;
-    public Motorcycle(){}
-    public Motorcycle(int driverIntensity) => this.driverIntensity = driverIntensity;
+    //public Motorcycle() { }
+    //public Motorcycle(int driverIntensity) => this.driverIntensity = driverIntensity; // This will lead us to redundant constructor so we will use the this keyword.
+
+    // create all kind of constructor
+    public Motorcycle() {  }
+    //public Motorcycle(): this(0, "") { Console.WriteLine("This is default constructor"); } // Instead of this we can just do this
+    //public Motorcycle(int intensity): this(intensity, "") { Console.WriteLine("This is intensity constructor"); }
+    //public Motorcycle(string name): this(0, name) { Console.WriteLine("This is name constructor"); }
+    public Motorcycle(int intensity = 10, string name = "") { // Optional arguments is way more better then this
+        if (intensity < 10) intensity = 10;
+        this.driverIntensity = intensity;
+        this.driverName = name;
+
+        Console.WriteLine("This is master constructor");
+    }
 
     public void PopAWheely()
     {
-        for(int i = 0; i < driverIntensity; i++) Console.WriteLine("Hurrra");
-    }    
+        for (int i = 0; i < driverIntensity; i++) Console.WriteLine("Hurrra");
+    }
     public void SetDriverName(string driverName) => this.driverName = driverName; // We know what is meaning of "this" keyword, to repreesnt the current class context
 
 }
--- SimpleClassExample/SimpleClassExample/Program.cs
+++ SimpleClassExample/SimpleClassExample/Program.cs
@@ -1,19 +1,21 @@
 ﻿using SimpleClassExample;
 
-Car car = new Car();
+Car car = new Car(234, "Shubham");
 Car car2; // We will have compilation error 
 //car2.petName = "SHubham Kumar";
-car.currSpeed = 21;
-car.petName = "Hot Pink Brezza";
+//car.currSpeed = 21;
+//car.petName = "Hot Pink Brezza";
 
-for (int i = 0; i < 10; i++)
-{
-    car.SpeedUp(3);
-    car.PrintState();
-}
+//for (int i = 0; i < 10; i++)
+//{
+//    car.SpeedUp(3);
+//    car.PrintState();
+//}
 
 // We cand do this
-car2 = new Car();
-bool flag;
-Car car3 = new Car(100, "OD", out flag);
-car3.PrintState();
+//car2 = new Car();
+//bool flag;
+//Car car3 = new Car(100, "OD", out flag);
+//car3.PrintState();
+
+Motorcycle mt1 = new Motorcycle(120);
