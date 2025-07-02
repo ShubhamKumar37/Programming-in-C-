521e2828fe45e3228d6b379ca8f2d6eb9ca071b4
Shubham Kumar <107699951+ShubhamKumar37@users.noreply.github.com>
2025-07-02 18:06:50 +0530

    studied about throw keyword and some exception moving to try-block

---
+++ ObjectOverrides/ObjectOverrides/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOverrides
+{
+
+    public class Person
+    {
+        public string FirstName {  get; set; }
+        public string LastName{  get; set; }
+        public int Age{  get; set; }
+        public Person() { }
+        public Person(int Age, string FirstName, string LastName) {
+            this.Age = Age;
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+        }
+
+        public override string ToString() => $"[FirstName: {FirstName}, LastName: {LastName}, Age: {Age}]";
+        
+        public override bool Equals(object obj)
+        {
+            //if(!(obj is Person temp)) return false;
+            //if (!(this.FirstName == temp.FirstName) || !(this.LastName == temp.LastName) || !(this.Age == temp.Age)) return false;
+            //return true;
+            return obj?.ToString() == this.ToString();
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            Console.WriteLine("ToString: {0}", p1.ToString());
+            Console.WriteLine("Hash code: {0}", p1.GetHashCode());
+            Console.WriteLine("Type: {0}", p1.GetType());
+            Person p2 = p1;
+            object o = p2;
+
+            if (o.Equals(p1) && p2.Equals(o))
+            {
+                Console.WriteLine("Same instance!");
+            }
+
+            Console.WriteLine(p2.ToString());
+        }
+    }
+}
---
+++ SimpleException/SimpleException/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleException
+{
+    class Car
+    {
+        public const int Maxspeed = 100;
+        public int CurrSped { get; set; } = 0;
+        public string PetName { get; set; } = "";
+        private bool _isCarDead = false;
+        private readonly Radio _theMusicBox = new Radio();
+
+        public Car() { }
+        public Car(int speed, string petName)
+        {
+            PetName = petName;
+            CurrSped = speed;   
+        }
+
+        public void CrankTunes(bool state)
+        {
+            _theMusicBox.TurnOn(state);
+        }
+
+        public void Accelerate(int delta)
+        {
+            if (_isCarDead == true) Console.WriteLine("Already dead car");
+            else
+            {
+                CurrSped += delta;
+                if (CurrSped > Maxspeed)
+                {
+                    CurrSped = 0;
+                    _isCarDead = true;
+                    //Console.WriteLine("{0} has been overheated", PetName); // Instead of this i can throw exception using throw keyword
+                    throw new Exception($"{PetName} has been overheated");
+                }
+                Console.WriteLine("{0} is the current speed", CurrSped);
+            }
+
+
+        }
+
+    }
+
+    class Radio
+    {
+        public void TurnOn(bool on)
+        {
+            Console.WriteLine(on == true ? "Jamming" : "Quite Time");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Simple Exception Example *****");
+            Console.WriteLine("=> Creating a car and stepping on it!");
+            Car myCar = new Car(20, "Zippy");
+            myCar.CrankTunes(true);
+            for (int i = 0; i < 10; i++)
+            {
+                myCar.Accelerate(10);
+            }
+            myCar.CrankTunes(true);
+        }
+    }
+}
