97b103850753fe07ef59f50069dec5737bf60b11
Shubham Kumar <107699951+ShubhamKumar37@users.noreply.github.com>
2025-07-03 21:38:48 +0530

    done with custom exception

---
+++ CustomException/CustomException/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomException
+{
+    class Car
+    {
+        public const int Maxspeed = 100;
+        public int CurrSped { get; set; } = 0;
+        public string PetName { get; set; } = "";
+        private bool _isCarDead = false;
+        private readonly Radio _theMusicBox = new Radio();
+
+        public Car() { }
+        public Car(int speed, string petName)
+        {
+            PetName = petName;
+            CurrSped = speed;
+        }
+
+        public void CrankTunes(bool state)
+        {
+            _theMusicBox.TurnOn(state);
+        }
+
+        public void Accelerate(int delta)
+        {
+            if (_isCarDead == true) Console.WriteLine("Already dead car");
+            else
+            {
+                CurrSped += delta;
+                if (CurrSped > Maxspeed)
+                {
+                    CurrSped = 0;
+                    _isCarDead = true;
+                    //Console.WriteLine("{0} has been overheated", PetName); // Instead of this i can throw exception using throw keyword
+                    throw new CarIsDeadException($"{PetName} has been overheated")
+                    {
+                        HelpLink = " http://www.cars.com/overheated",
+                        Data =
+                        {
+                            { "TimeStamps", $"Car was dead at {DateTime.Now}" },
+                            {"Cause", "You had lead the foot" }
+                        }
+                    };
+                }
+                Console.WriteLine("{0} is the current speed", CurrSped);
+            }
+
+
+        }
+
+    }
+
+    class Radio
+    {
+        public void TurnOn(bool on)
+        {
+            Console.WriteLine(on == true ? "Jamming" : "Quite Time");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Car car1 = new Car(21, "Nin");
+
+            try
+            {
+                car1.Accelerate(323);
+            }
+            catch(CarIsDeadException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            //Console.WriteLine("***** Simple Exception Example *****");
+            //Console.WriteLine("=> Creating a car and stepping on it!");
+            //Car myCar = new Car(20, "Zippy");
+            //myCar.CrankTunes(true);
+            //myCar.CrankTunes(true);
+
+            //try
+            //{
+
+            //    for (int i = 0; i < 10; i++)
+            //    {
+            //        myCar.Accelerate(10);
+            //    }
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine("\n*** Error! ***");
+            //    Console.WriteLine("Member name: {0}", e.TargetSite);
+            //    Console.WriteLine("Class defining member: {0}", e.TargetSite.DeclaringType);
+            //    Console.WriteLine("Help Link: {0}", e.HelpLink);
+            //    Console.WriteLine("Member type: {0}", e.TargetSite.MemberType);
+            //    Console.WriteLine("Message: {0}", e.Message);
+            //    Console.WriteLine("Source: {0}", e.Source);
+
+            //    foreach (DictionaryEntry i in e.Data)
+            //    {
+            //        Console.WriteLine($"This is key = {i.Key} and its value is {i.Value}");
+            //    }
+
+            //}
+            //NullReferenceException nullRefEx = new NullReferenceException();
+            //Console.WriteLine(
+            //"NullReferenceException is-a SystemException? : {0}",
+            //nullRefEx is SystemException);
+        }
+    }
+}
