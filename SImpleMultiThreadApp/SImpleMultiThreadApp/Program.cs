646fa5c655d72882dfa3866ffe0a1d3ab4db42a7
Shubham Kumar <107699951+ShubhamKumar37@users.noreply.github.com>
2025-07-19 17:03:51 +0530

    working with threading

---
+++ SImpleMultiThreadApp/SImpleMultiThreadApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+
+Thread currThread = Thread.CurrentThread;
+currThread.Name = "Main Thread";
+Printer p = new Printer();
+
+Thread secondThread = new Thread(new ThreadStart(p.PrintNumber));
+secondThread.Name = "Second thread";
+secondThread.Start();
+public class Printer
+{
+    public void PrintNumber()
+    {
+        Console.WriteLine("-> {0} is executing PrintNumbers()", Thread.CurrentThread.Name);
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(" -> {0} is printing number {1}", Thread.CurrentThread.Name, i);
+            Thread.Sleep(100);
+        }
+    }
+}
