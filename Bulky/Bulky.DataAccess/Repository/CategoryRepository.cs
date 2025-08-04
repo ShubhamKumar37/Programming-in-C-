6402309f91fcd1c957e655e0851320c03a627b40
Shubham Kumar <107699951+ShubhamKumar37@users.noreply.github.com>
2025-08-04 17:57:05 +0530

    now working with repository pattern

---
+++ Bulky/Bulky.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,49 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        void IRepository<Category>.Add(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        Category IRepository<Category>.Get(Expression<Func<Category, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerable<Category> IRepository<Category>.GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IRepository<Category>.Remove(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IRepository<Category>.RemoveRange(IEnumerable<Category> entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        void ICategoryRepository.Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        void ICategoryRepository.Update(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Bulky/Bulky.DataAccess/Repository/Repository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Reposittory.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bulky.DataAccess.Reposittory
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly AppDbContext _context;
+        internal DbSet<T> dbSet;
+        public Repository(AppDbContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            this.dbSet = _context.Set<T>();
+            // this is equivalent to _context.Set<T>() but allows for better readability and context.Category
+        }
+        void IRepository<T>.Add(T entity)
+        {
+            _context.Add(entity);
+        }
+
+        T IRepository<T>.Get(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = dbSet;
+            query = query.Where(filter);
+            return query.FirstOrDefault();
+        }
+
+        IEnumerable<T> IRepository<T>.GetAll()
+        {
+            IQueryable<T> query = dbSet;
+            return query.ToList();
+        }
+
+        void IRepository<T>.Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        void IRepository<T>.RemoveRange(IEnumerable<T> entity)
+        {
+            dbSet.RemoveRange(entity);
+        }
+    }
+}
